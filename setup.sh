6f4aa479127212a07d0c03d8c51c1556a346f04f
aechiara <aechiara@gmail.com>
2015-02-06 13:43:11 -0200

    added client_max_body_size to nginx.conf

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ echo "cgi.fix_pathinfo=0" >> /etc/php5/fpm/php.ini
 cp joomla /etc/nginx/sites-available/default
 cp joomla /etc/nginx/sites-enabled/default
 chown -R www-data:www-data /var/www/joomla
+sed -ie "/sendfile on;/a\\
+client_max_body_size 10M;" /etc/nginx/nginx.conf
